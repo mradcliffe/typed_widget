edb268c22db06f1482c8c66ee5ade142f3fdb845
Matthew Radcliffe <mradcliffe@softpixel.com>
2016-09-23 20:45:28 -0400

    Issue none by mradcliffe: Fix coding standards on ElementBuilderInterface

--- src/ElementBuilderInterface.php
+++ src/ElementBuilderInterface.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
- * @file
- * Contains ElementBuilderInterface
- */
 
 namespace Drupal\typed_widget;
 
 use Drupal\Core\TypedData\DataDefinitionInterface;
 
 /**
- *
+ * Provides an interface for classes that are called from TypedElementBuilder.
  */
 interface ElementBuilderInterface {
 
@@ -18,7 +14,8 @@ interface ElementBuilderInterface {
    * Get the element type from the data definition.
    *
    * @param \Drupal\Core\TypedData\DataDefinitionInterface $definition
-   *   The data definition
+   *   The data definition.
+   *
    * @return string
    *   The element type.
    */
@@ -30,11 +27,13 @@ interface ElementBuilderInterface {
    * @param string $type
    *   The element type.
    * @param \Drupal\Core\TypedData\DataDefinitionInterface $definition
-   *   The data definition
+   *   The data definition.
    * @param string $parent_type
    *   An optional parent element type.
+   *
    * @return array
    *   An array of element properties to add to the element array.
    */
   public static function getProperties($type, DataDefinitionInterface $definition, $parent_type = '');
+
 }
